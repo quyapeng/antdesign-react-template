46bd387b46d334321b571452f00367560195db4c
qu <1095698022@qq.com>
2022-06-04 22:38:31 +0800

    update: form

--- src/pages/system/menu/index.tsx
+++ src/pages/system/menu/index.tsx
@@ -1,11 +1,9 @@
 import React, { useState, useMemo, useEffect, useRef } from 'react';
 import { menuList } from '@/services/api';
-import Tree, { DataNode } from 'antd/lib/tree';
+import Tree from 'antd/lib/tree';
 import useRequest from '@ahooksjs/use-request';
 import { Button, Card, Col, Form, Input, Radio, Row } from 'antd';
-import { TreeNode } from 'antd/lib/tree-select';
 import Icon from '@ant-design/icons';
-import { ProFormInstance } from '@ant-design/pro-form';
 
 const Menu: React.FC = () => {
   /**
@@ -50,25 +48,9 @@ const Menu: React.FC = () => {
     });
     console.log(detail);
     setDetail(detail);
+    setTitle('编辑菜单');
     form?.setFieldsValue(detail);
-    // this.detail = detail;
     // this.id = detail.id;
-    // this.type = 'edit';
-    // this.title = '编辑菜单';
-    // const {
-    //   id: detailid,
-    //   parent,
-    //   authority,
-    //   name,
-    //   code,
-    //   path,
-    //   type,
-    //   icon,
-    //   seq,
-    //   isShow,
-    //   isDelete,
-    //   status,
-    // } = detail;
   };
   const onFinish = (values: any) => {
     console.log('Success:', values);
@@ -76,6 +58,10 @@ const Menu: React.FC = () => {
 
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
+  };
+  const addHandle = () => {
+    setTitle('新增菜单');
+    form?.resetFields();
   };
   const treeData = [
     {
@@ -97,6 +83,9 @@ const Menu: React.FC = () => {
   return (
     <Row className="menu">
       <Col span={12}>
+        <Button type="primary" key="primary" onClick={addHandle}>
+          新增菜单
+        </Button>
         <Card title="菜单数据" bordered={false}>
           <Tree
             onSelect={onSelect}
@@ -113,33 +102,6 @@ const Menu: React.FC = () => {
             }}
           />
         </Card>
-
-        {/* <a-icon slot="icon" type="carry-out" /> */}
-        {/* {treeData?.map((item: any) => {
-            return (
-              <TreeNode key={item.id} value={item.title} treeData=""> */}
-        {/* <Icon slot="icon" type="carry-out" />
-                <span slot="title" style="{color: #1890ff}">
-                  {item.name}
-                </span>
-                {item.subMenus.map((i: any) => {
-                  return (
-                    <TreeNode key={i.id} title={i.name} value={''}>
-                      <Icon slot="icon" type="carry-out" />
-                      {i.subMenus.map((q: any) => {
-                        return (
-                          <TreeNode key={q.id} title={q.name} value={''}>
-                            <Icon slot="icon" type="carry-out" />
-                          </TreeNode>
-                        );
-                      })}
-                    </TreeNode>
-                  );
-                })} */}
-        {/* </TreeNode> */}
-        {/* ); */}
-        {/* })} */}
-        {/* </Tree> */}
       </Col>
       <Col span={12}>
         <Card title={title} bordered={false}></Card>
@@ -152,41 +114,41 @@ const Menu: React.FC = () => {
           onFinishFailed={onFinishFailed}
           autoComplete="off"
         >
-          <Form.Item label="父级" name="username" rules={[{ required: false }]}>
+          <Form.Item label="父级" name="parentId" rules={[{ required: false }]}>
             <Input placeholder="请选择菜单父级" />
           </Form.Item>
-          <Form.Item label="名称" name="username" rules={[{ required: true, message: '' }]}>
+          <Form.Item label="名称" name="name" rules={[{ required: true, message: '' }]}>
             <Input placeholder="请输入名称" />
           </Form.Item>
           <Form.Item label="编号" name="code" rules={[{ required: true, message: '' }]}>
             <Input placeholder="请输入名称" />
           </Form.Item>
-          <Form.Item label="类型" name="code" rules={[{ required: true, message: '' }]}>
+          <Form.Item label="类型" name="type" rules={[{ required: true, message: '' }]}>
             <Radio.Group>
-              <Radio value="0">菜单</Radio>
-              <Radio value="1">权限</Radio>
+              <Radio value="MENU">菜单</Radio>
+              <Radio value="ACTION">权限</Radio>
             </Radio.Group>
           </Form.Item>
-          <Form.Item label="路由" name="code" rules={[{ required: false }]}>
+          <Form.Item label="路由" name="path" rules={[{ required: false }]}>
             <Input placeholder="请输入路由" />
           </Form.Item>
-          <Form.Item label="权限" name="code" rules={[{ required: false }]}>
+          <Form.Item label="权限" name="authority" rules={[{ required: false }]}>
             <Input placeholder="请输入权限" />
           </Form.Item>
-          <Form.Item label="图标" name="code" rules={[{ required: false }]}></Form.Item>
-          <Form.Item label="排序" name="code" rules={[{ required: true, message: '' }]}>
+          <Form.Item label="图标" name="icon" rules={[{ required: false }]}></Form.Item>
+          <Form.Item label="排序" name="seq" rules={[{ required: true, message: '' }]}>
             <Input placeholder="请输入菜单排序" />
           </Form.Item>
-          <Form.Item label="是否展示" name="code" rules={[{ required: true, message: '' }]}>
+          <Form.Item label="是否展示" name="show" rules={[{ required: true, message: '' }]}>
             <Radio.Group>
-              <Radio value="0">展示</Radio>
-              <Radio value="1">不展示</Radio>
+              <Radio value={true}>展示</Radio>
+              <Radio value={false}>不展示</Radio>
             </Radio.Group>
           </Form.Item>
-          <Form.Item label="状态" name="code" rules={[{ required: true, message: '' }]}>
+          <Form.Item label="状态" name="status" rules={[{ required: true, message: '' }]}>
             <Radio.Group>
-              <Radio value="0">有效</Radio>
-              <Radio value="1">无效</Radio>
+              <Radio value="ENABLED">有效</Radio>
+              <Radio value="DISABLED">无效</Radio>
             </Radio.Group>
           </Form.Item>
           <Form.Item wrapperCol={{ offset: 8, span: 16 }}>

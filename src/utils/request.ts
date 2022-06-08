459048da5a49e65d99d4f2fd4ee7381ede225de8
qu <1095698022@qq.com>
2022-06-08 22:28:16 +0800

    update: request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,39 +1,3 @@
-// import axios from 'axios';
-// import { message } from 'antd';
-// import { xsrfHeaderName, AUTH_TYPE } from '@/constant/index';
-
-// const request = axios.create({
-//   baseURL: 'https://dev-api.qlion.com',
-//   xsrfHeaderName,
-//   timeout: 5000,
-//   withCredentials: true,
-// });
-
-// request.interceptors.response.use((option: any) => {
-//   console.log('option', option);
-//   let { status, statusText, data } = option;
-//   if (status == '200') {
-//     return data;
-//   } else {
-//     message.error(statusText);
-//   }
-//   //   return option;
-// });
-// //请求拦截
-// request.interceptors.request.use((option: any) => {
-//   let { url } = option;
-//   //获取token
-//   const token = localStorage.getItem(xsrfHeaderName) || '';
-//   if (url === '/login/oauth/token') {
-//     option.headers[xsrfHeaderName] = `${AUTH_TYPE.BASIC} Y2xpZW50OjEyMzQ1Ng==`;
-//     option.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-//   } else {
-//     option.headers[xsrfHeaderName] = `${token}`;
-//   }
-//   return option;
-// });
-
-// export default request;
 import { message } from 'antd';
 import { xsrfHeaderName, AUTH_TYPE } from '@/constant/index';
 import { codeMessage } from '@/constant/error';
@@ -43,7 +7,6 @@ const errorHandler = (error: any) => {
   const { response = {} } = error;
   const errortext = codeMessage[response.status] || response.statusText;
   const { status, url } = response;
-  debugger;
 
   message.error({
     content: `请求错误 ${status}: ${errortext}`,
